9bdd5fb7430062690822554e9948e5fa5d5bc6dc
Mousumi Dutta <97080528+MousumiDutta2000@users.noreply.github.com>
2023-04-26 11:39:42 +0530

    Update urls.py

--- dashboard/dashboard/urls.py
+++ dashboard/dashboard/urls.py
@@ -21,7 +21,8 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
-
+    
+    # Django Rest Framework
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('user_list/', UserViewSet.as_view({'get': 'list'}), name='user_list'),
