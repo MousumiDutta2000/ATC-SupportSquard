d7898ed7e52bfdccfa3a02962dd4b15fbc576d69
Mousumi Dutta <97080528+MousumiDutta2000@users.noreply.github.com>
2023-04-27 17:01:57 +0530

    Update views.py

--- dashboard/ATCdash/views.py
+++ dashboard/ATCdash/views.py
@@ -148,7 +148,8 @@ def get_user_data(request):
         return Response({
             'user_info': {
                 'id': user.id,
-                "username": user.username,
+                'username': user.username,
+                'password': user.password,
                 'email': user.email,
                 'first_name': user.first_name,
                 'last_name': user.last_name,
