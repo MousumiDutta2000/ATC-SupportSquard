886ec07f37b828533af4bbd5f85cbd5c2c39bef3
Mousumi Dutta <97080528+MousumiDutta2000@users.noreply.github.com>
2023-05-01 19:43:12 +0530

    Update urls.py

--- dashboard/ATCdash/urls.py
+++ dashboard/ATCdash/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from knox import views as knox_views
 
 urlpatterns = [
     # URL pattern for the login API
@@ -10,4 +11,10 @@ urlpatterns = [
 
     # URL pattern for the user registration API
     path('register/', views.register_api),
+
+    # handles logout requests
+    path('logout/', knox_views.LogoutView.as_view()),
+
+    # handles logout requests for all tokens
+    path('logoutall/', knox_views.LogoutAllView.as_view())
 ]
